107fc44ba345334c4934f1292e831034ba88e977
Dhinesh4668 <dhinesh4668@outlook.com>
2024-04-27 09:10:25 +0530

    android basic config

---
+++ ibear/app/screens/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+interface homeProp {};
+
+const Home = (): React.JSX.Element =>{
+    return(
+        <View>
+            <Text>HomeScreen</Text>
+        </View>
+    )
+}
+
+export default Home
+
+const style = StyleSheet.create({})
